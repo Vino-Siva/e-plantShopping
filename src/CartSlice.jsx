a8ece76260d9f9e8aac95fcfdd3c0e68a13e95ea
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-12-21 13:12:19 +0530

    Improved CartSlice reducers

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,44 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const CreatSlice = createSlice({
+export const CartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
-    totalItems: 0,
   },
   reducers: {
     addItem: (state, action) => {
       const { name, image, cost } = action.payload;
-      const existingItem = state.items.find((item) => item.name === name);
-      if (existingItem) {
-        existingItem.quantity++;
+      const index = state.items.findIndex((item) => item.name === name);
+      if (index !== -1) {
+        state.items[index].quantity++;
       } else {
         state.items.push({ name, image, cost, quantity: 1 });
       }
-      state.totalItems++;
     },
     removeItem: (state, action) => {
-      const { name } = action.payload;
-      const itemToRemove = state.items.find((item) => item.name === name);
-
-      if (itemToRemove) {
-        state.totalItems -= itemToRemove.quantity;
-        state.items = state.items.filter((item) => item.name !== name);
+      const index = state.items.findIndex(
+        (item) => item.name === action.payload
+      );
+      if (index !== -1) {
+        state.items.splice(index, 1);
       }
     },
     updateQuantity: (state, action) => {
-      const { name, quantity } = action.payload;
-      const itemToUpdate = state.items.find((item) => item.name === name);
-      if (itemToUpdate) {
-        state.totalItems += quantity - itemToUpdate.quantity;
-        itemToUpdate.quantity = quantity;
+      const index = state.items.findIndex(
+        ({ name }) => name === action.payload.name
+      );
+      if (index !== -1) {
+        state.items[index].quantity = action.payload.quantity;
       }
     },
   },
 });
 
-export const selectTotalItems = (state) => state.cart.totalItems;
+export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
-export const { addItem, removeItem, updateQuantity } = CreatSlice.actions;
-
-export default CreatSlice.reducer;
+export default CartSlice.reducer;
